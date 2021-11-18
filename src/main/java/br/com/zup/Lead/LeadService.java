0f35df265a0992c47e302c257d3f44b13d2f7a5a
Larissa Novais <larinovais54@gmail.com>
2021-11-18 13:27:00 -0300

    Criar Metodo para atualizar lead

--- src/main/java/br/com/zup/Lead/LeadService.java
+++ src/main/java/br/com/zup/Lead/LeadService.java
@@ -1,10 +1,13 @@
 package br.com.zup.Lead;
 
 import br.com.zup.Lead.model.Lead;
+import br.com.zup.Lead.model.Produto;
 import br.com.zup.Lead.respository.LeadRepository;
 import br.com.zup.Lead.respository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LeadService {
@@ -21,4 +24,15 @@ public class LeadService {
         produtoRepository.saveAll(lead.getProdutos());
         return salvarLead(lead);
     }
+
+    public Lead verificarLead() {
+
+    }
+
+    public void atualizarListaDeProduto(List<Produto> produtos, Lead lead) {
+        for (Produto produtoRef : produtos) {
+            lead.getProdutos().add(produtoRef);
+        }
+    }
+
 }
